13e743c79a01e278a84d4b82798e2c25154a9001
Arthur Cheng <arthurpk1209@gmail.com>
2024-06-05 11:15:53 +0800

    Update lcm_download.sh

--- lcm_download.sh
+++ lcm_download.sh
@@ -9,56 +9,63 @@
 
 cmd='curl -O'
 
-# LCM
+# LCM_FRAMEWORK_BUNDLE
 $cmd http://download.nutanix.com/lcm/3.0-H/lcm_dark_site_bundle_3.0.48302-H.tar.gz
 
-# latest compatibility & signature files 
+# LCM_COMPATIBILITY_SIGNATURE 
 $cmd https://download.nutanix.com/product-meta/lcm/nutanix_compatibility_bundle.tar.gz
 
-# Foundation
+# FOUNDATION
 $cmd http://download.nutanix.com/Foundation/5.6.0.1/lcm_foundation_5.6.0.1.tar.gz
+# FOUNDATION_PLATFORM
 $cmd http://download.nutanix.com/Foundation/Platforms/2.15.0.1/foundation-platforms-2.15.0.1.tar.gz
+# FOUNDATION_CENTRAL
 $cmd http://download.nutanix.com/foundation_central/releases/1.6/lcm_foundation-central_1.6.tar.gz
 
-# Firmware
+# FIRMWARE
 $cmd http://download.nutanix.com/Platforms/lcm-builds/nx/dark-site/3.6.0/lcm_nx_3.6.0.tar.gz
 #$cmd http://download.nutanix.com/Platforms/lcm-builds/hpe/dark-site/1.9.2/lcm_hpe_1.9.2.tar.gz
 #$cmd http://download.nutanix.com/Platforms/lcm-builds/dell/dark-site/2.8.0/lcm_dell_2.8.0.tar.gz
 
-# Files
+# FILE_SERVER
 # Customers running AOS 6.5.x and Files 4.2.x - 4.4.x, with the CVM and File Server [Client & Storage network] on the same network should not upgrade to Files 5.0
 $cmd http://download.nutanix.com/afs/5.0.0/lcm_file_server_5.0.0.tar.gz
+# FSM
 $cmd http://download.nutanix.com/fsm/5.0.0/lcm_fsm_5.0.0.tar.gz
+# FILES_MANAGER
 $cmd http://download.nutanix.com/fm/5.0.0/lcm_files_manager_5.0.0.tar.gz
 
-# File Analytics
+# FILES_ANALYTICS
 # 3.4.0.1 -> Ref KB16681
 $cmd http://download.nutanix.com/fileanalytics/3.4.0.1/lcm_file_analytics_3.4.0.1.tar.gz
 
-# Objects
+# OBJECTS
 $cmd http://download.nutanix.com/NutanixObjects/5.0/objects-5.0.tar.gz
 
-# Flow Network Security
+# FLOW_PC
 # FNS Release Version 4.1.0 is not an independent release. FNS Release Version 4.1.0 is bundled with PC 2024.1.
 $cmd http://download.nutanix.com/FlowPC/4.1.0/lcm_flow_pc_4.1.0.tar.gz
+# FLOW_PE
 $cmd http://download.nutanix.com/FlowCVM/4.1.0/lcm_flow_cvm_4.1.0.tar.gz
 
-# Flow Virtual Networking
+# FLOW_VIRTUAL_NETWORKING
 $cmd http://download.nutanix.com/AdvancedNetworking/lcm-builds/darksite/4.0.0.tar.gz
+# NETWORK_GATEWAY
+$cmd http://download.nutanix.com/networkGateway/6.4.170fcf6.20240318/vyos_6.4.170fcf6.20240318.zip
 
-# Nutanix Kubernetes Engine
+# NKE
 $cmd http://download.nutanix.com/karbon/2.10.0/lcm_karbon_2.10.0.tar.gz
 
 # AOS STS 6.8.x & AHV
-$cmd http://download.nutanix.com/releases/6.8/lcm_nos_6.8.tar.gz
-$cmd http://download.nutanix.com/hypervisor/ahv/lcm-builds/darksite/el8.nutanix.20230302.100173/lcm_ahv_el8.nutanix.20230302.100173.tar.gz
+$cmd http://download.nutanix.com/releases/6.8.0.1/lcm_nos_6.8.0.1.tar.gz
+$cmd http://download.nutanix.com/hypervisor/ahv/lcm-builds/darksite/el8.nutanix.20230302.100187/lcm_ahv_el8.nutanix.20230302.100187.tar.gz
 
 # AOS LTS 6.5.x & AHV
 $cmd http://download.nutanix.com/releases/6.5.5.6/lcm_nos_6.5.5.6.tar.gz
 $cmd http://download.nutanix.com/hypervisor/ahv/lcm-builds/darksite/el7.nutanix.20220304.480/lcm_ahv_el7.nutanix.20220304.480.tar.gz
 
-# Prism Central
-$cmd http://download.nutanix.com/pc/pc.2024.1/lcm_pc_pc.2024.1.tar.gz
+# PC
+$cmd http://download.nutanix.com/pc/pc.2024.1.0.1/lcm_pc_pc.2024.1.0.1.tar.gz
 
 # CMU
 $cmd http://download.nutanix.com/cmu/lcm-builds/darksite/2.0.3/lcm_cmu_2.0.3.tar.gz
